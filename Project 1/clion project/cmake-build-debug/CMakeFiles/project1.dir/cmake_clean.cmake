46de2947baa3d2c65d6d8866cc77781d809974fa
Thanasis Charisoudis <thanasis.harisoudis@gmail.com>
2019-09-27 20:22:38 +0300

    no change

---
+++ Project 1/clion project/cmake-build-debug/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/main.c.o"
+  "CMakeFiles/project1.dir/sampling_gettimeofday.c.o"
+  "CMakeFiles/project1.dir/stats.c.o"
+  "CMakeFiles/project1.dir/sample_predictive.c.o"
+  "CMakeFiles/project1.dir/linearregression.c.o"
+  "project1.pdb"
+  "project1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
