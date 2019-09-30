92ff5a1e73fa3f06c799591d288492a4c36cdbe2
Thanasis Charisoudis <thanasis.harisoudis@gmail.com>
2019-09-30 21:38:13 +0300

    many minor changes in WebReport

--- Project 2 - Final/report/Timeline/server.sh
+++
@@ -1,2 +0,0 @@
-google-chrome http://localhost:8000/Timeline </dev/null >/dev/null 2>&1 & disown
-cd .. && python -m SimpleHTTPServer
---
+++ Project 2 - Final/report/WebReport/server.sh
@@ -0,0 +1,2 @@
+cd .. && python -m SimpleHTTPServer&
+google-chrome http://localhost:8000/WebReport </dev/null >/dev/null 2>&1 & disown
